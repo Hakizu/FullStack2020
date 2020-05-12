d1c506090f99f7f20e1a9f0c5e5beb52e45c1d0b
Hakizu <35292022+Hakizu@users.noreply.github.com>
2020-05-12 13:04:34 +0200

    1.7-1.9 Unicafe step 2+3+4.js

---
+++ part 1/Javascript/1.7-1.9 Unicafe step 2+3+4.js
@@ -0,0 +1,52 @@
+import React, {useState} from 'react'
+import ReactDOM from 'react-dom'
+
+const Button = ({onClick, text}) =>
+    <button onClick={onClick}>
+      {text}
+    </button>
+
+const Statistics = ({good, neutral, bad}) => {
+  if (good+neutral+bad ===0) {return <p>No Feedback given</p>}
+  if (good+neutral+bad > 0) {
+  return(
+      <p>
+      Good {good} <br/>
+      Neutral {neutral} <br/>
+      Bad {bad} <br/>
+      Average {(good * 1 + bad * (-1)) / (good + neutral + bad)} <br/>
+      Positive {good * 1 / bad * (-1)}
+      </p>
+  )
+}
+}
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGood = () => {
+    setGood(good + 1)
+  }
+  const handleNeutral = () => {
+    setNeutral(neutral + 1)
+  }
+  const handleBad = () => {
+    setBad(bad + 1)
+  }
+
+  return (
+    <div>
+      <Button onClick={handleGood} text="Good" />
+      <Button onClick={handleNeutral} text="Neutral"/>
+      <Button onClick={handleBad} text="Bad"/>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <App />, 
+  document.getElementById('root')
+)
